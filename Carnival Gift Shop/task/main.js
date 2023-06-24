c8d88584b8661fa5739bcdd989cf90e0f37c64d4
Yamuna Kumari <yamunakumari961@gmail.com>
2023-06-24 22:32:06 +0530

    stage 2

--- Carnival Gift Shop/task/main.js
+++ Carnival Gift Shop/task/main.js
@@ -1,8 +1,18 @@
 // write your code here
-const gifts = ['Teddy Bear', 'Big Red Ball', 'Huge Bear', 'Candy', 'Stuffed Tiger', 'Stuffed Dragon', 'Skateboard', 'Toy Car',"Basketball", "Scary Mask"]
+const gifts = [
+    {name: 'Teddy Bear', price: 10, id: 1},
+    {name: 'Big Red Ball', price: 5, id: 2},
+    {name: 'Huge Bear', price: 50, id: 3},
+    {name: 'Candy', price: 8, id: 4},
+    {name: 'Stuffed Tiger', price: 15, id: 5},
+    {name: 'Stuffed Dragon', price: 30, id: 6},
+    {name: 'Skateboard', price: 100, id: 7},
+    {name: 'Toy Car', price: 25, id: 8},
+    {name: "Basketball", price: 20, id: 9},
+    {name: "Scary Mask", price: 75, id: 10}]
 
 console.log("WELCOME TO THE CARNIVAL GIFT SHOP!")
 console.log("Hello friend! Thank you for visiting the carnival!")
 console.log("Here's the list of gifts:")
 console.log()
-gifts.forEach(item => console.log(item))
+gifts.forEach(item => console.log(`${item.id}- ${item.name}, Cost: ${item.price} tickets`))
--- Carnival Gift Shop/task/tests.py
+++ Carnival Gift Shop/task/tests.py
@@ -9,16 +9,16 @@ class CarnivalGiftShopTest(StageTest):
     welcome_msg = "WELCOME TO THE CARNIVAL GIFT SHOP!"
     greet_msg = "Hello friend! Thank you for visiting the carnival!"
     gift_list = """Here's the list of gifts:\n
-Teddy Bear
-Big Red Ball
-Huge Bear
-Candy
-Stuffed Tiger
-Stuffed Dragon
-Skateboard
-Toy Car
-Basketball
-Scary Mask"""
+1- Teddy Bear, Cost: 10 tickets
+2- Big Red Ball, Cost: 5 tickets
+3- Huge Bear, Cost: 50 tickets
+4- Candy, Cost: 8 tickets
+5- Stuffed Tiger, Cost: 15 tickets
+6- Stuffed Dragon, Cost: 30 tickets
+7- Skateboard, Cost: 100 tickets
+8- Toy Car, Cost: 25 tickets
+9- Basketball, Cost: 20 tickets
+10- Scary Mask, Cost: 75 tickets"""
 
     # test if the output is correct
     @dynamic_test
